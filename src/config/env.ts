ccfe36e6096c6a091bb252990228186aeaed2b77
Ushnish Dey Bhowmik <neilbhowmik05@gmail.com>
2026-05-20 18:23:20 +0530

    WIP: Prisma Setup

--- src/config/env.ts
+++ src/config/env.ts
@@ -4,10 +4,12 @@ dotenv.config({
     path: `.env.${process.env.NODE_ENV || 'development'}`,
 });
 
-export const env = {
+const env = {
     NODE_ENV: process.env.NODE_ENV || 'development',
     PORT: process.env.PORT || 3000,
     DATABASE_URL: process.env.DATABASE_URL || '',
     REDIS_HOST: process.env.REDIS_HOST || 'localhost',
     REDIS_PORT: process.env.REDIS_PORT ? parseInt(process.env.REDIS_PORT, 10) : 6379,
 }
+
+export default env;
--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import cors from "cors";
 import helmet from "helmet";
 import morgan from "morgan";
-import { env } from './config/env';
+import env from "./config/env.js";
 
 const app = express();
 
